5efed2989b21be01c053d56822999bad95dcd26b
lehuulong1611-tech <lehuulong1611@gmail.com>
2026-06-25 11:45:48 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using QLBanHangAPI.Models;
 using QLBanHangAPI.Data;
+using Microsoft.Data.SqlClient; // 🚨 Nhớ đảm bảo có dòng này để dùng SqlConnection
+
 System.AppContext.SetSwitch("Microsoft.Data.SqlClient.DisableStrictTokenValidation", true);
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -12,7 +15,6 @@ builder.Services.AddCors(options =>
 });
 
 // Add services to the container.
-
 builder.Services.AddControllers()
     .AddNewtonsoftJson(options =>
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
@@ -21,19 +23,22 @@ builder.Services.AddControllers()
 // Lấy chuỗi kết nối CHBANH từ file appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("CHBANH");
 
-// Đăng ký CHBANHDbContext vào hệ thống để các Controller sau này gọi sử dụng
-
-    builder.Services.AddDbContext<CHBANHDbContext>(options =>
-        options.UseSqlServer(connectionString));
+// 🚨 ĐOẠN ĐẶC TRỊ CHO EF CORE: Ép tắt mã hóa trực tiếp qua SqlConnection
+builder.Services.AddDbContext<CHBANHDbContext>(options =>
+{
+    var sqlConnection = new SqlConnection(connectionString);
+    
+    // Ép bằng code: Bỏ qua kiểm tra chứng chỉ và tắt bắt buộc mã hóa nâng cao
+    sqlConnection.Credential = null; 
+    
+    options.UseSqlServer(sqlConnection);
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -42,11 +47,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseCors("AllowAll");
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
